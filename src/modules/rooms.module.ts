5ced44b1110e2a4a6b976a6d39380d9aa855d93f
Herrold Real <herrold.real@go1.com>
2023-03-15 18:32:21 -0600

    Websocket test

--- src/modules/rooms.module.ts
+++ src/modules/rooms.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { jwtModule, redisModule } from 'src/modules/module.config';
+import { jwtModule, redisModule } from './module.config';
 import { RoomService } from '../services/room/room.service';
 import { RoomsRepository } from '../services/room/room.repository';
 import { RoomGateway } from '../websocket/gateway/room.gateway';
