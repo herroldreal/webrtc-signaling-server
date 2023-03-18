f8be37dd244ce8147c92f3037eb4ce333766630a
Herrold Real <herrold.real@go1.com>
2023-03-17 20:30:07 -0600

    Websocket Signaling

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -1,14 +1,14 @@
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { Get, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Body, Controller, Post } from '@nestjs/common';
 import { RoomService } from '../services/room/room.service';
 
-@UsePipes(new ValidationPipe())
-@Controller('rooms')
+//@UsePipes(new ValidationPipe())
+@Controller('rtc')
 export class RoomController {
   constructor(private roomService: RoomService) {}
 
-  @Post()
+  @Get('/')
   async create(@Body() dto: any) {
-    return await this.roomService.createRoom(dto);
+    // return await this.roomService.createRoom(dto);
   }
 }
