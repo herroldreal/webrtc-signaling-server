f54474bf5510d7c8ea79a0795cd520050b7deb2f
Herrold Real <herrold.real@go1.com>
2023-03-17 21:09:18 -0600

    Websocket Signaling

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -1,6 +1,7 @@
-import { Get, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { RoomService } from '../services/room/room.service';
+import { Response } from 'express';
 
 //@UsePipes(new ValidationPipe())
 @Controller('rtc')
@@ -8,7 +9,10 @@ export class RoomController {
   constructor(private roomService: RoomService) {}
 
   @Get('/')
-  async create(@Body() dto: any) {
-    // return await this.roomService.createRoom(dto);
+  async create(@Res({ passthrough: true }) res: Response) {
+    res.status(HttpStatus.SWITCHING_PROTOCOLS).json({
+      status: 'OK',
+      message: 'Switching Protocols',
+    });
   }
 }
