173142749fc1a7f066c66ef5796bc0146189124c
Herrold Real <herrold.real@go1.com>
2023-04-01 12:10:31 -0600

    🚀 chore(terraform.yml): update Terraform version to 4.0 🔒 chore(provider-variables.tf): remove aws_access_key and aws_secret_key variables 🔒 chore(provider-main.tf): remove aws_access_key and aws_secret_key variables from provider block 🔒 chore(terraform.tfvars): remove aws_access_key and aws_secret_key variables The Terraform version has been updated to 4.0 in the terraform.yml file. The aws_access_key and aws_secret_key variables have been removed from the provider-variables.tf, provider-main.tf, and terraform.tfvars files. This is because the AWS provider now uses the default AWS credentials chain to authenticate, which is more secure.

--- .infra/provider-main.tf
+++ .infra/provider-main.tf
@@ -7,13 +7,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
+    }
+  }
+
+  cloud {
+    organization = "personal-herrold"
+
+    workspaces {
+      name = "webrtc-android-personal"
     }
   }
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region     = var.aws_region
 }
