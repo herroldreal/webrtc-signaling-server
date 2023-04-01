0179e365495798df9a01d110d1345d31f42d1a2e
Herrold Real <herrold.real@go1.com>
2023-04-01 12:26:15 -0600

    🔒 chore(provider-main.tf): add AWS profile to provider configuration 📈 chore(provider-main.tf): update AWS provider version to 3.74.0 The AWS provider version has been updated to 3.74.0 to ensure compatibility with the latest version of the provider. The AWS profile has been added to the provider configuration to allow for the use of named profiles when authenticating with AWS.

--- .infra/provider-main.tf
+++ .infra/provider-main.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "3.74.0"
     }
   }
 
@@ -21,5 +21,6 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
+  profile = "default"
+  region  = var.aws_region
 }
